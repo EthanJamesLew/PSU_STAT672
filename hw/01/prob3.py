08859fe922d52bf95e4bf972780576ba72ccd430
Ethan Lew <elew@galois.com>
2020-01-27 10:35:37 -0800

    fix hw01 error in inner product notation 2.2

--- hw/01/prob3.py
+++ hw/01/prob3.py
@@ -1,7 +1,15 @@
+"""
+Code for STAT 672 HW 1, Prob 3
+
+Ethan Lew
+(elew@pdx.edu)
+"""
 
 import numpy as np
 from scipy.special import jv 
+import matplotlib.pyplot as plt
 
+# For LaTeX Plots
 #from matplotlib import rc
 #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 #rc('text', usetex=True)
@@ -21,12 +29,12 @@ def k_tanh(x, xp, kappa, Theta):
 def k_bessel(x, xp, n, v, sigma):
     return jv(v, sigma*np.sum((x-xp)**2))/(np.sum((x-xp)**2)**(-float(n*(v+1))))
 
-
 def k_min(x, xp):
     return np.min([x, xp])
 
 def kernel_mat(f, x):
-    '''
+    ''' Given a kernel f and a collection of observations x, construct the matrix
+    [K]_{ij} = f(x_i, x_j)
     :param f: kernel function
     :param x: vector of values
     :return: K symmetric matrix
@@ -51,7 +59,12 @@ def sample_functions(m, N, kernel):
     K = kernel_mat(kernel, X)
     return X, np.random.multivariate_normal(np.zeros((2*m+1)), K, (N))
 
-def plot_samples(ax, f, title):
+def plot_samples(ax, X, f, title):
+    """ Given a plt.axis object, plot f and give it a title 
+    :param ax: pyplot axis object
+    :param f: n samples of length l
+    :param title: string for axis title 
+    """
     # Plot Results
     for fi in f:
         ax.plot(X, fi)
@@ -59,8 +72,7 @@ def plot_samples(ax, f, title):
     ax.set_ylabel(r"$f_i(x)$")
     ax.set_xlim((np.min(X), np.max(X)))
 
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
+def main():
     m = 10 # +/- range of X
     tau = 2 # tau Gaussian kernel parameter
     N = 10 # Number of samples
@@ -69,16 +81,16 @@ if __name__ == "__main__":
     X, f1 = sample_functions(m, N, lambda x,y: k_gaussian(x, y, 10))
     X, f2 = sample_functions(m, N, lambda x,y: k_polynomial(x, y, 2))
     X, f3 = sample_functions(m, N, lambda x,y: k_min(x, y))
-    
+   
     # Plot Results
     figg, axg = plt.subplots((2), sharex=True, figsize=(4,8))
     figk, axk = plt.subplots((2), sharex=True, figsize=(4,8))
-    plot_samples(axg[0], f0, r"Samples for $\tau = 3$")
-    plot_samples(axg[1], f1, r"Samples for $\tau = 10$")
+    plot_samples(axg[0], X, f0, r"Samples for $\tau = 3$")
+    plot_samples(axg[1], X, f1, r"Samples for $\tau = 10$")
     axg[1].set_xlabel(r"$x$")
     
-    plot_samples(axk[0], f2, r"Samples for Polynomial Kernel $d=2$")
-    plot_samples(axk[1], f3, r"Samples for $K(x, y) =\operatorname{min}(x, y)$")
+    plot_samples(axk[0], X, f2, r"Samples for Polynomial Kernel $d=2$")
+    plot_samples(axk[1], X, f3, r"Samples for $K(x, y) =\operatorname{min}(x, y)$")
     axk[1].set_xlabel(r"$x$")
 
     plt.figure(figg.number)
@@ -87,4 +99,7 @@ if __name__ == "__main__":
     plt.savefig("img/other_kernel_fig.eps")
 
     plt.show()
+
+if __name__ == "__main__":
+    main()
 
